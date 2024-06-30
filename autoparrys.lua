e93a198f288f690761ad7e052fd68b97aa0f9955
darkbisam <darkbisam@gmail.com>
2024-07-01 02:30:35 +0800

    Update autoparrys.lua

--- autoparrys.lua
+++ autoparrys.lua
@@ -1,3 +1,1 @@
--- IDK the Name, made by 1 F0
-_G.UI_Size = 200
-loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/-beta-/main/AutoParry.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/darkbisam/Autoparrys/main/SensatioXs.lua"))()
