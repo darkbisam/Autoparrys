918d01703abb8503b519eff7b6e160c50c78f94e
darkbisam <darkbisam@gmail.com>
2024-07-01 01:37:39 +0800

    Update autoparrys.lua

--- autoparrys.lua
+++ autoparrys.lua
@@ -1,14 +1,18 @@
-while _G.Toggle do wait()
-    for i,v in pairs(game:GetService("Workspace")["__THINGS"].Lootbags:GetChildren()) do
-    v.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
+while _G.Toggle do
+    wait()
+    for i, v in pairs(game:GetService("Workspace")["__THINGS"].Lootbags:GetChildren()) do
+        v.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
     end
 end
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("SensatioX Hub | update BETA V1.2")
-local Tab = Window:NewTab("version")
-local Tab = Window:NewTab("settings")
-local Section = Tab:NewSection("open")
-Section:NewButton("Auto Block", "Auto Block", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/AutoBlock"))()
+
+local VersionTab = Window:NewTab("version")
+local SettingsTab = Window:NewTab("settings")
+
+local SettingsSection = SettingsTab:NewSection("open")
+SettingsSection:NewButton("Auto Block", "Auto Block", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/AutoBlock"))()
     print("Clicked")
 end)
