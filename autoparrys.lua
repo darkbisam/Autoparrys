f8a1b048b23d1f018fcc4b1aed82e13ab0c242f6
darkbisam <darkbisam@gmail.com>
2024-07-01 15:55:40 +0800

    Update autoparrys.lua

--- autoparrys.lua
+++ autoparrys.lua
@@ -1,1 +1,15 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/darkbisam/Autoparrys/main/SensatioXs.lua"))()
+local ContextActionService = game:GetService('ContextActionService')
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
+local function open_case(action_name)
+    if not ReplicatedStorage.Remotes then
+        return
+    end
+
+    local case_type = action_name
+
+    ReplicatedStorage.Remotes.Store:WaitForChild(`RequestOpen{case_type}Box`):InvokeServer()
+end
+
+ContextActionService:BindAction('Sword', open_case, true, Enum.KeyCode.X)
+ContextActionService:BindAction('Explosion', open_case, true, Enum.KeyCode.V)
